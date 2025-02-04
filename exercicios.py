9da437575625e34c174824bbda373d32b605de85
joaoemmanuel <joao.basass@gmail.com>
2025-02-04 15:37:18 -0300

    Concluí o sexto exercício

--- exercicios.py
+++ exercicios.py
@@ -63,19 +63,27 @@
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
-transacao = {"valor": 12000, 'hora': 20}
-if transacao['valor'] > 10000 or not 9 <= transacao['hora'] <= 18:
-    print("Transação suspeita.")
-else:
-    print("Transação bem sucedida")
-# elif not 9 <= transacao['hora'] <= 18:
-#     print("Transação suspeita pelo horário de operação.")
+# transacao = {"valor": 12000, 'hora': 20}
+# if transacao['valor'] > 10000 or not 9 <= transacao['hora'] <= 18:
+#     print("Transação suspeita.")
 # else:
-#     print("Transação bem sucedida.")
-
+#     print("Transação bem sucedida")
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
+texto = str(input("Digite o texto: "))
+palavras = texto.split()
+contagem = {}
+
+for palavra in palavras:
+    if palavra in contagem:
+        contagem[palavra] += 1
+    else:
+        contagem[palavra] = 1
+
+print(contagem)
+
+
 
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
