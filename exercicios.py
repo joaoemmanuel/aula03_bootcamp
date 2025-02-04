3bf2397b85101fc0e263c2aa59e21c8f416f8240
joaoemmanuel <joao.basass@gmail.com>
2025-02-04 10:59:39 -0300

    Concluí o segundo exercício

--- exercicios.py
+++ exercicios.py
@@ -3,22 +3,33 @@
 # que todos os registros tenham valores positivos para `quantidade` e `preço`. 
 # Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
 # forem positivos ou "Dados inválidos" caso contrário.
-try:
-    preco = int(input("Insira o valor: "))
-    qtd   = int(input("Insira a quantidade: "))
-    if preco > 0 and qtd > 0:
-        print("Dados válidos")
-    else:
-        print("Dados inválidos") 
-except ValueError:
-    print("O valor informado não foi um número inteiro.")
-
-    
+# try:
+#     preco = int(input("Insira o valor: "))
+#     qtd   = int(input("Insira a quantidade: "))
+#     if preco > 0 and qtd > 0:
+#         print("Dados válidos")
+#     else:
+#         print("Dados inválidos") 
+# except ValueError:
+#     print("O valor informado não foi um número inteiro.")
 
 ### Exercício 2: Classificação de Dados de Sensor
 # Imagine que você está trabalhando com dados de sensores IoT. 
 # Os dados incluem medições de temperatura. Você precisa classificar cada leitura 
 # como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+# Temperatura < 18°C é 'Baixa'
+# Temperatura >= 18°C e <= 26°C é 'Normal'
+# Temperatura > 26°C é 'Alta'
+try:
+    temp = float(input("Informe a temperatura: "))
+    if temp < 18:
+        print(f"A temperatura de {temp}°C é considerada baixa.")
+    elif temp >= 18 and temp <= 26:
+        print(f"A temperatura de {temp}°C é considerada normal.")
+    else:
+        print(f"A temperatura de {temp}°C é considerada alta.")
+except ValueError:
+    print("O valor informado não é válido.")
 
 ### Exercício 3: Filtragem de Logs por Severidade
 # Você está analisando logs de uma aplicação e precisa filtrar mensagens 
