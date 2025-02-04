a469bb0a3af31dbd3babec2462cb41c5b7e6c4e7
joaoemmanuel <joao.basass@gmail.com>
2025-02-04 17:13:45 -0300

    Concluí o décimo exercício

--- exercicios.py
+++ exercicios.py
@@ -117,6 +117,21 @@
 
 ### Exercício 10. Agregação de Dados por Categoria
 # Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
+vendas = [
+    {"categoria": "eletrônicos", "valor": 1200},
+    {"categoria": "livros", "valor": 200},
+    {"categoria": "eletrônicos", "valor": 800}
+]
+agregado = {}
+for venda in vendas:
+    categoria = venda["categoria"]
+    valor = venda["valor"]
+    if categoria in agregado:
+        agregado[categoria] += valor
+    else:
+        agregado[categoria] = valor
+
+print(agregado)
 
 ### Exercícios com WHILE
 
